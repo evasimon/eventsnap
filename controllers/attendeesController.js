fd79fec6c8c30d63db4ab0ef77b89d1c6bc81533
Eva Simon <eva.simon@gmail.com>
2017-11-23 21:38:18 -0700

    sets article controller

---
+++ controllers/attendeesController.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+
+// Defining methods for the attendeeController
+module.exports = {
+    // writes the new attendee to the database
+    create: function (req, res) {
+        db.attendee
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+
+    },
+    // finds all the attendees in the database
+    findAll: function (req, res) {
+        db.attendee
+            .findAll(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    // removes attendee from the database
+    remove: function (req, res) {
+        db.attendee
+            .find({ where: {id: req.params.id }})
+            .then(dbModel => dbModel.destroy())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
