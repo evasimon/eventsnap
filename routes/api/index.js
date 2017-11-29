07d72fe68930cbfa9e5e08443d0629ddac754e47
Eva Simon <eva.simon@gmail.com>
2017-11-29 15:02:23 -0600

    adds checkin route and sets put method on the server

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const attendeesRoutes = require("./attendees");
+const checkInRoutes = require("./checkIn");
+
 
 // attendees routes
 router.use("/attendees", attendeesRoutes);
+router.use("/check-in", checkInRoutes)
 
 module.exports = router;
