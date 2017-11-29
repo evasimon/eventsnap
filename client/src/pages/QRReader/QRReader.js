c5b94719aaeadd90316a0b38835cf303b926f342
Eva Simon <eva.simon@gmail.com>
2017-11-29 15:01:25 -0600

    sets up handleCheckIn function on the client

--- client/src/pages/QRReader/QRReader.js
+++ client/src/pages/QRReader/QRReader.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
 import { Col, Row, Container } from "../../components/Grid";
 import QrReader from "react-qr-reader";
+import API from "../../utils/API";
+
 
 class QRReader extends Component {
 
@@ -10,20 +12,36 @@ class QRReader extends Component {
         this.state = {
             delay: 300,
             result: 'No result',
+            checkInStatus: 0
         }
         this.handleScan = this.handleScan.bind(this)
     }
 
     handleScan(data) {
+        console.log(data)
         if (data) {
             this.setState({
                 result: data,
             })
+            this.handleCheckIn(data, 2)
         }
     }
 
     handleError(err) {
         console.error(err)
+    }
+
+    handleCheckIn = (uuid, id) => {
+        API.checkIn(uuid, id)
+            .then(res => {
+                console.log(`${uuid} is checked in now to workshop ${id}`)
+                console.log('send me the data', res.data)
+                console.log(res.data.workshopAttending.checkedIn)
+                this.setState({
+                    checkInStatus: res.data.workshopAttending.checkedIn
+                })
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
@@ -39,6 +57,7 @@ class QRReader extends Component {
                                 style={{ width: '40%' }}
                             />
                             <p>{this.state.result}</p>
+                            <p>{this.state.checkInStatus}</p>
                         </Jumbotron>
                     </Col>
                 </Row>
