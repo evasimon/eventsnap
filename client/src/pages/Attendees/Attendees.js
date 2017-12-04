aa3d029fe5b984e889ed2ec9ddd1efda90f92383
Eva Simon <eva.simon@gmail.com>
2017-12-04 15:53:26 -0600

    sets ckeckmark state

--- client/src/pages/Attendees/Attendees.js
+++ client/src/pages/Attendees/Attendees.js
@@ -21,6 +21,41 @@ export default class Attendees extends Component {
             .catch(err => console.log(err))
     }
 
+    attendeeRender() {
+        return this
+            .state
+            .attendees
+            .map(attendee => (
+                <List.Item className='card' key={attendee.id}>
+                    <List.Icon name='checkmark' size='big' verticalAlign='middle' color={attendee.registered
+                        ? "green"
+                        : "grey"} // {{attendee.registered} ? 'color = green' : 'color = grey'}
+                    />
+                    <List.Content>
+                        <List.Header as='h2'>
+                            {attendee.firstName}
+
+                            {attendee.lastName}
+                        </List.Header>
+                        <List.Description>
+                            <List>
+                                <List.Item icon='users' content='Dancer Type'/>
+                                <List.Item icon='marker' content='New York, NY'/>
+                                <List.Item
+                                    icon='mail'
+                                    content=
+                                    {<a href='mailto:jack@semantic-ui.com'> jack@semantic - ui.com </a>}/>
+                                <List.Item
+                                    icon='linkify'
+                                    content=
+                                    {<a href='http://www.semantic-ui.com'> semantic-ui.com </a>}/>
+                            </List>
+                        </List.Description>
+                    </List.Content>
+                </List.Item>
+            ))
+    }
+
     render() {
 
         return (
@@ -28,35 +63,7 @@ export default class Attendees extends Component {
                 {this.state.attendees.length
                     ? (
                         <List divided inverted relaxed className='one-card'>
-                            {this
-                                .state
-                                .attendees
-                                .map(attendee => (
-                                    <List.Item className='card' key={attendee.id}>
-                                        <List.Icon name='checkmark' size='big' verticalAlign='middle' color={'grey'}/>
-                                        <List.Content>
-                                            <List.Header as='h2'>
-                                                {attendee.firstName}
-
-                                                {attendee.lastName}
-                                            </List.Header>
-                                            <List.Description>
-                                                <List>
-                                                    <List.Item icon='users' content='Dancer Type'/>
-                                                    <List.Item icon='marker' content='New York, NY'/>
-                                                    <List.Item
-                                                        icon='mail'
-                                                        content=
-                                                        {<a href='mailto:jack@semantic-ui.com'> jack@semantic - ui.com </a>}/>
-                                                    <List.Item
-                                                        icon='linkify'
-                                                        content=
-                                                        {<a href='http://www.semantic-ui.com'> semantic-ui.com </a>}/>
-                                                </List>
-                                            </List.Description>
-                                        </List.Content>
-                                    </List.Item>
-                                ))}
+                            {this.attendeeRender()}
 
                         </List>
                     )
