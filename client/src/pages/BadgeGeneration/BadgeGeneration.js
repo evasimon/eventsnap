39f3b810d8f9e1589c23a439860a7892af410503
Eva Simon <eva.simon@gmail.com>
2017-12-05 00:22:43 -0600

    updates component to print badges and adds styling

--- client/src/pages/BadgeGeneration/BadgeGeneration.js
+++ client/src/pages/BadgeGeneration/BadgeGeneration.js
@@ -33,30 +33,21 @@ class BadgeGeneration extends Component {
                                 .state
                                 .attendees
                                 .map(attendee => (
-                                    <List.Item key={attendee.id} className='card'>
-                                        <Image
-                                            className="qrImage"
-                                            src=
-                                            { `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${attendee.uuid}`}
-                                            size='small'
-                                            alt="this is a unique QR code"/>
-                                        < List.Content >
+                                    <List.Item key={attendee.id} className='badge-card'>
+                                        <List.Content>
+                                            <Image
+                                                className="qrImage"
+                                                src=
+                                                {'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + attendee.uuid}
+                                                size='small'
+                                                alt="this is a unique QR code"/>
                                             <List.Header as='h2'>
-                                                {attendee.firstName}
-                                                {attendee.lastName}
+                                                {attendee.firstName + " " + attendee.lastName}
                                             </List.Header>
                                             <List.Description>
                                                 <List>
-                                                    <List.Item icon='users' content='Dancer Type'/>
-                                                    <List.Item icon='marker' content='New York, NY'/>
-                                                    <List.Item
-                                                        icon='mail'
-                                                        content=
-                                                        {<a href='mailto:jack@semantic-ui.com'> jack@semantic - ui.com </a>}/>
-                                                    <List.Item
-                                                        icon='linkify'
-                                                        content=
-                                                        {<a href='http://www.semantic-ui.com'> semantic-ui.com </a>}/>
+                                                    <List.Item content={attendee.city + ", " + attendee.state}/>
+                                                    <List.Item content='Full Festival Pass'/>
                                                 </List>
                                             </List.Description>
                                         </List.Content>
